4d6cf3be4befcf20e7b66a1177a150a507733296
zhouweihny <zhouweihny@163.com>
2019-02-21 17:22:49 +0800

    1

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="mainpage" id="mainpage">
-    <!-- 顶部中间标题 -->
-    <!-- <dashTit></dashTit> -->
+    <div class="zui-flex J_weather">
+      <div>2019年02月16日</div>
+      <div>6℃ ~ 11℃</div>
+      <div>雨</div>
+    </div>
+    <div class="zui-flex J_parkNum">
+      <div>车辆在园：100</div>
+      <div>剩余车位：20</div>
+    </div>
     <!-- 中间内容区 -->
     <div class="J_main" id="J_main">
       <h3 class="J_roomrent_tit">房间租赁数据</h3>
@@ -23,6 +30,11 @@
       <div class="item J_workorder">
         <workorder></workorder>
       </div>
+
+      <div class="item J_zdevice">
+        <zdevice></zdevice>
+      </div>
+
     </div>
   </div>
 </template>
@@ -35,6 +47,7 @@ import roomrent from '@/components/roomrent'
 import yearroomrent from '@/components/yearroomrent'
 import monthroomrent from '@/components/monthroomrent'
 import workorder from '@/components/workorder'
+import zdevice from '@/components/zdevice'
 
 export default {
   name: 'home',
@@ -44,6 +57,7 @@ export default {
     yearroomrent,
     monthroomrent,
     workorder,
+    zdevice,
     page1
   },
   data () {
@@ -72,6 +86,8 @@ a {
 }
 </style>
 <style scoped lang="scss">
+$base_colo: #7bb9dc;
+
 .mainpage {
   background: url(../../static/bj.jpg) left top no-repeat;
   // width: 1920px;
@@ -84,6 +100,25 @@ a {
   position: absolute;
   left: 0;
   top: 0;
+}
+
+.J_weather, .J_parkNum {
+  height: 50px;
+  line-height: 50px;
+  position: absolute;
+  top: 80px;
+  justify-content: space-around;
+  align-items: center;
+  color: $base_colo;
+  font-size: 30px;
+}
+.J_weather {
+  width: 500px;
+  left: 840px;
+}
+.J_parkNum {
+  width: 460px;
+  left: 2100px;
 }
 
 .J_main {
@@ -113,6 +148,12 @@ a {
       top: 1504px;
       width: 766px;
       height: 300px;
+    }
+    &.J_zdevice {
+      left: 1087px;
+      top: 320px;
+      width: 780px;
+      height: 190px;
     }
   }
   .J_roomrent_tit {
---
+++ src/components/zdevice.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="zui-flex cwrap">
+    <h3 class="J_zdevice_tit">设备在线情况</h3>
+    <div></div>
+    
+  </div>
+</template>
+<script>
+import echarts from 'echarts'
+
+export default {
+  name: 'roomrent',
+  data() {
+    return {
+      msg: 'roomrent',
+      name: '房间租赁数据',
+      legendArr: [],
+      color: [],
+      myChart: {},
+    }
+  },
+  mounted() {
+
+    
+  },
+  methods: {
+    
+  }
+}
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+$base_colo: #7bb9dc;
+
+.cwrap {
+  position: relative;
+  .J_zdevice_tit {
+    position: absolute;
+    top: -20px;
+    left: -60px;
+    font-size: 30px;
+    color: #5CEAFB;
+  }
+}
+
+.arrow {
+  width: 13px;
+  height: 17px;
+  display: inline-block;
+  margin-left: 3px;
+
+  &.up {
+    background: url(../../static/up.png) 0 0 no-repeat;
+    background-size: contain;
+  }
+
+  &.down {
+    background: url(../../static/down.png) 0 0 no-repeat;
+    background-size: contain;
+  }
+}
+
+</style>
